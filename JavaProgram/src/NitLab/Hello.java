fc33c9a76c09df6663fbef3caee4978b756963df
Admin <Admin@ASIF>
2025-09-18 07:29:09 +0530

    s

---
+++ JavaProgram/src/NitLab/Hello.java
@@ -0,0 +1,15 @@
+package NitLab;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b = 20;
+		int s = a+b;
+		
+		System.out.println("SUm is : "+s);
+				
+
+	}
+
+}
