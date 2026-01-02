2f2e72843d8c90adcc964c6c0b76ccb1a42e8561
S-Asif11 <Admin@ASIF>
2026-01-02 20:02:02 +0530

    c

---
+++ JavaProgram/src/logicbuilding/conditionalthinking/PositiveNegative.java
@@ -0,0 +1,31 @@
+package logicbuilding.conditionalthinking;
+
+import java.util.Scanner;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		//check a number is positive negative or zero?
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = Integer.parseInt(sc.nextLine());
+		
+		
+		if(num == 0) {
+			System.out.println("Number is 0");
+		}
+		else if(num>0) {
+			if((num/2)*2 == num) {
+				System.out.println("Number is positive");
+			}
+			else System.out.println("number is negative");
+		}
+		
+		else System.out.println("Number is negative");
+		
+		sc.close();
+		
+
+	}
+
+}
